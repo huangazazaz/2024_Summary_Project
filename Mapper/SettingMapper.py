567094742497dd1204a77e0be0aa8c3c127d94a7
solovesonxi <152047212+solovesonxi@users.noreply.github.com>
2024-07-15 12:03:56 +0800

    Update SettingMapper.py

--- Mapper/SettingMapper.py
+++ Mapper/SettingMapper.py
@@ -1,21 +1,26 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from DTO.SettingDTO import Setting  # 确保 UserDTO 中定义了 User 类，且该类继承自 Base
+from sqlalchemy.orm import sessionmaker
+
+from DTO.SettingDTO import Setting
 
 Base = declarative_base()
 
 
 class SettingMapper:
     def __init__(self):
-        # 请根据实际数据库驱动和凭证修改下面的连接字符串
         self.engine = create_engine('mysql+pymysql://root:hhelibeb@localhost:3306/summary')
         Base.metadata.create_all(self.engine)
         self.session = sessionmaker(bind=self.engine)()
 
     def add(self, setting: Setting):
-        self.session.add(setting)
-        self.session.commit()
+        try:
+            self.session.add(setting)
+            self.session.commit()
+            return 'success'
+        except Exception as e:
+            self.session.rollback()
+            return 'fail'
 
     def get(self, user_id):
         setting = self.session.query(Setting).filter(Setting.user_id == user_id).first()
